a1b3ad06e2fef5c9f63c6da9af769dbf0356c31b
sama888-i <samaki-bp215@code.edu.az>
2024-10-09 10:01:50 +0400

    first commit

---
+++ task1.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 8, 4, 36, 2, 9 };
+            int min = Min(numbers);
+            Console.WriteLine( min);
+        }
+        static int Min(int[] arr)
+        {
+            int min = arr[0] ;
+            for (int i = 1;i< arr.Length;i++)
+            {
+
+                if (arr[i] < min)  
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+
+        }
+    }
+}
---
+++ task2.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence ="Mence Sehvdi ";
+            basherf(sentence);
+            
+        }
+        static void  basherf(string sentence)
+        {
+            if (sentence.Length >0)
+            {
+                Console.Write(sentence[0]);
+
+            }
+            for (int i =1; i< sentence.Length; i++)
+            {
+                if (sentence[i -1] ==' ' && sentence[i] !=' ')
+                {
+                    Console.Write(sentence[i]);
+                }
+            }
+
+        }
+
+    }
+}
